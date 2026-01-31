e83477cc08f9cc97653eac9bae68402e3cb905b9
Alex <alexandrumacsim@yahoo.com>
2026-01-31 21:48:17 +0100

    Completed exercise 3 from chapter 1.

--- ch1/hello.go
+++ ch1/hello.go
@@ -2,8 +2,16 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	fmt.Println("Hello, World!")
+// }
+
+// Exercise 3
 func main() {
-	fmt.Println("Hello, World!")
+	// Can't end with new line
+	fmt.Println("Hello, World! \n\bAnother Line")
+	fmt.Println("	Second print	\bNow what?	Asd")
+	// fmt.Println("something\n") // This will cause a vet error but not a compiler error
 }
 
 // testing go vet
