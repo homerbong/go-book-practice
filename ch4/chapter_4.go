c7ebcd7386b1188846915aa1d4908b239a6b1380
Alex <alexandrumacsim@yahoo.com>
2026-04-24 09:54:15 +0100

    Went through the for block statements.

--- ch4/chapter_4.go
+++ ch4/chapter_4.go
@@ -25,6 +25,61 @@ func example42() {
 	fmt.Println("Again x = ", x)
 }
 
+/*
+Idiomatic way of writing the FizzBuzz logic.
+Avoid nesting ifs. Use continue to make code clearer
+*/
+func example412() {
+	fmt.Println("FizzBuzz example")
+	for i := 1; i <= 50; i++ {
+		if i != 1 {
+			fmt.Print(", ")
+		}
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Print("FizzBuzz")
+			continue
+		}
+		if i%3 == 0 {
+			fmt.Print("Fizz")
+			continue
+		}
+		if i%5 == 0 {
+			fmt.Print("Buzz")
+			continue
+		}
+		fmt.Print(i)
+	}
+
+	fmt.Println()
+}
+
+func example415() {
+	fmt.Println("The order of keys is not guaranteed in Go")
+	m := map[string]int{
+		"a": 1,
+		"c": 3,
+		"b": 2,
+	}
+
+	for i := 0; i < 3; i++ {
+		fmt.Println("Loop", i)
+		for k, v := range m {
+			fmt.Println(k, v)
+		}
+	}
+}
+
+func example416() {
+	fmt.Println("Strings iterate over runes, not over bytes. The first value indicates the number of bytes from the start though.")
+	samples := []string{"hello", "apple_π!"}
+	for _, sample := range samples {
+		for i, r := range sample {
+			fmt.Println(i, r, string(r))
+		}
+		fmt.Println()
+	}
+}
+
 func shadowingPackages() {
 	fmt.Println()
 	fmt.Println("Shadowing packages:")
@@ -75,6 +130,169 @@ func ifBlockExamples() {
 	// fmt.Println(x) // compiler error: undefined: x
 }
 
+func forBlockExamples() {
+	// complete for statement
+	fmt.Println("Complete for statement: ")
+	fmt.Print("i -> ")
+	for i := 0; i < 10; i += 1 {
+		fmt.Print(i)
+		if i < 9 {
+			fmt.Print(", ")
+		}
+	}
+	fmt.Println()
+	fmt.Println()
+
+	idx := 0
+	fmt.Println("for statement without initialization")
+	fmt.Print("i -> ")
+	for ; idx < 10; idx += 2 {
+		fmt.Print(idx)
+		if idx < 8 {
+			fmt.Print(", ")
+		}
+	}
+	fmt.Println()
+	fmt.Println("idx at the end: ", idx) // idx is being declared outside and it's being modified
+	fmt.Println()
+
+	fmt.Println("for statement without increment")
+	fmt.Print("i -> ")
+	for i := 0; i < 10; {
+		fmt.Print(i)
+		if i == 0 {
+			i++
+		} else {
+			i += 2
+		}
+
+		if i <= 9 {
+			fmt.Print(", ")
+		}
+	}
+	fmt.Println()
+	fmt.Println()
+
+	// The condition only for statement
+	idx = 0
+	fmt.Println("Condition only for statement")
+	fmt.Print("i -> ")
+	for idx < 10 { // no semicolons needed
+		fmt.Print(idx)
+		if idx < 8 {
+			fmt.Print(", ")
+		}
+		idx += 2
+	}
+	fmt.Println()
+	fmt.Println()
+
+	// The infinite for loop
+	idx = 1
+	fmt.Println("The infinite for statement")
+	for {
+		if n := rand.Intn(20); n == 18 {
+			fmt.Println()
+			fmt.Printf("Interrupting the for loop because the random number is 18 after %d cycles", idx)
+			break
+		}
+		idx++
+	}
+	fmt.Println()
+
+	example412()
+	fmt.Println()
+
+	// The for range statement
+	fmt.Println("The for range statement")
+	evenVals := []int{2, 4, 6, 8, 10}
+	indexes := ""
+	values := ""
+	for i, v := range evenVals {
+		indexes = indexes + string(fmt.Sprint(i))
+		values = values + string(fmt.Sprint(v))
+		if i < len(evenVals)-1 {
+			indexes = indexes + ", "
+			values = values + ", "
+		}
+	}
+	fmt.Println("indexes: ", indexes)
+	fmt.Println("values : ", values)
+
+	wins := map[string]int{"Fred": 1, "Mike": 4, "Alex": 2}
+	for k, v := range wins {
+		fmt.Println(k, ": ", v)
+	}
+
+	values = ""
+	for _, v := range evenVals { // You can ignore the first variable with underscore
+		values = values + string(fmt.Sprint(v))
+		if v != evenVals[len(evenVals)-1] {
+			values = values + ", "
+		}
+	}
+	fmt.Println("values: ", values)
+
+	keys := make([]string, 0, 10)
+	for k := range wins {
+		keys = append(keys, k)
+	}
+	fmt.Println("keys: ", keys)
+
+	fmt.Println()
+	example415()
+	fmt.Println()
+
+	fmt.Println("Iterating over strings")
+	example416()
+
+	fmt.Println("For values are a copy")
+	for _, v := range evenVals { // This is created on each iteration. Useful for go routines.
+		v *= 2
+	}
+	fmt.Println("Unchanged evenVals: ", evenVals)
+
+	// labels
+	fmt.Println("Labels")
+
+	samples := []string{"hello", "apple_π!"}
+outer:
+	for _, v := range samples {
+		fmt.Println(v)
+	inner:
+		for _, r := range v {
+			if r == 'e' || r == 'p' {
+				continue inner
+			}
+
+			if r == 'l' {
+				continue outer
+			}
+			fmt.Println(string(r))
+		}
+		fmt.Println()
+	}
+}
+
+func compareForLoops() {
+	evenVals := []int{2, 4, 6, 8, 10}
+	for i, v := range evenVals {
+		if i == 0 {
+			continue
+		}
+		if i == len(evenVals)-1 {
+			break
+		}
+		fmt.Println(i, v)
+	}
+
+	// VS
+
+	for i := 1; i < len(evenVals)-1; i++ { // not working for strings
+		fmt.Println(i, evenVals[i])
+	}
+}
+
 func main() {
 	display.SectionTitle("Chapter 4")
 	fmt.Println("hello")
@@ -102,4 +320,12 @@ func main() {
 
 	display.SectionTitle("If block")
 	ifBlockExamples()
+
+	display.SectionTitle("for block")
+	forBlockExamples()
+	// Use for range statement with built-in compound types as it makes thigs easier
+	// Use complete for statement when iterating over a subset of a compound type. (slices could also work?)
+	// Condition only is a good replacement for the while loop
+	// infinite for loop might be usefule in very rare cases and it should have a break or return statement. Also useful for the iterator pattern.
+
 }
