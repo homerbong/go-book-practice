ec6b9de9930e3b3f21c0fe9d477cbe6902a23f64
Alex <alexandrumacsim@yahoo.com>
2026-05-11 13:09:58 +0100

    Make minor changes to the make file, update the gitignore and work through the pointer declarations.

--- ch6/chapter6.go
+++ ch6/chapter6.go
@@ -1,9 +1,120 @@
 package main
 
-import "go-book-practice/display"
+import (
+	"fmt"
+	"go-book-practice/display"
+)
 
-func pointersExamples() {}
+type Foo struct {
+	foo string
+	bar int
+}
+
+type person struct {
+	FirstName  string
+	MiddleName *string
+	LastName   string
+}
+
+func makePointer[T any](t T) *T {
+	return &t
+}
+
+func pointerSyntaxExamples() {
+	x := "Hello"
+	pointerToX := &x
+
+	fmt.Println("x:", x)
+	fmt.Println("pointerToX:", pointerToX)
+
+	y := 10
+	pointerToY := &y
+	fmt.Println("y:", y)
+	fmt.Println("pointerToY:", pointerToY)
+	fmt.Println("*pointerToY:", *pointerToY)
+
+	z := 5 + *pointerToY
+	fmt.Println("z:", z)
+}
+
+func pointerTypeExamples() {
+	var x *int
+	fmt.Println("x == nil:", x == nil)
+	// Can't dereference before assigning
+	// fmt.Println("*x:", *x) // panic: runtime error: invalid memory address or nil pointer dereference
+
+	y := 10
+	var pointerToY *int
+	pointerToY = &y
+	fmt.Println("pointerToY:", pointerToY)
+	fmt.Println()
+
+	var z = new(int) // Instantiates a pointer to a zero value instance of the type.
+	fmt.Println("z != nil:", z != nil)
+	fmt.Println("*z:", *z)
+	fmt.Println()
+
+	str1 := struct {
+		name string
+		age  int
+	}{}
+	fmt.Println("str:", str1)
+	pointerToStruct := &struct {
+		name string
+		age  int
+	}{}
+	fmt.Println("pointerToStruct:", pointerToStruct)
+	fmt.Println("*pointerToStruct:", *pointerToStruct)
+	fmt.Println()
+
+	foo := &Foo{}
+	fmt.Println("foo:", foo)
+	fmt.Println("*foo:", *foo)
+	fmt.Println()
+
+	var primitive string
+	pointerToPrimitive := &primitive
+	fmt.Println("primitive:", primitive)
+	fmt.Println("pointerToPrimitive:", pointerToPrimitive)
+	fmt.Println()
+
+	// p := person {
+	// 	FirstName:  "Alex",
+	// 	MiddleName: "Perry", // build error: cannot use "Perry" (untyped string constant) as *string value in struct literal
+	// 	LastName:   "Peterson",
+	// }
+
+	// p := person{
+	// 	FirstName:  "Alex",
+	// 	MiddleName: &"Perry", // build error: invalid operation: cannot take address of "Perry" (untyped string constant)
+	// 	LastName:   "Peterson",
+	// }
+
+	// Alternative 1:
+	p := person{
+		FirstName:  "Alex",
+		MiddleName: pointerToPrimitive,
+		LastName:   "Peterson",
+	}
+	fmt.Println("p:", p)
+
+	// Alternative 2:
+	p = person{
+		FirstName:  "Alex",
+		MiddleName: makePointer("Perry"),
+		LastName:   "Peterson",
+	}
+	fmt.Println("p:", p)
+}
+
+func pointersExamples() {
+	pointerSyntaxExamples()
+	fmt.Println()
+
+	pointerTypeExamples()
+}
 
 func main() {
 	display.SectionTitle("Pointers")
+	pointersExamples()
 }
